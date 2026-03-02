47154995190f595599617caae9b8f0cb979ab169
José Carlos Pesina Siller <josecarlospesinasiller@Joses-MacBook-Air.local>
2026-03-02 23:38:34 +0400

    hotjar-id

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,18 +15,24 @@ const RootLayout = ({ children }: { children: ReactNode }) => (
   <html lang='en'>
     <head>
       {/* Hotjar Tracking Code for Ask WRI */}
-      <Script id='hotjar' strategy='afterInteractive'>
-        {`
-          (function(h,o,t,j,a,r){
-            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-            h._hjSettings={hjid:${process.env.NEXT_PUBLIC_HOTJAR_ID},hjsv:6};
-            a=o.getElementsByTagName('head')[0];
-            r=o.createElement('script');r.async=1;
-            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-            a.appendChild(r);
-          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-        `}
-      </Script>
+      {/* Hotjar Tracking Code for Ask WRI */}
+      {(() => {
+        const id = Number(process.env.NEXT_PUBLIC_HOTJAR_ID);
+        return id > 0 ? (
+          <Script id="hotjar" strategy="afterInteractive">
+            {`
+              (function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:${id},hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+              })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+            `}
+          </Script>
+        ) : null;
+      })()}
     </head>
     <body>
       <ChakraProvider>
