6983cd78a4804a862e1ffc36c698488486ad6d3f
Kinshuk Govil <Kinshuk.Govil@wri.org>
2026-03-06 11:43:55 -0800

    Temporarily move SSL policy back
    Next deployment should be able to upgrade it.

--- terraform/infrastructure/alb.tf
+++ terraform/infrastructure/alb.tf
@@ -51,7 +51,9 @@ resource "aws_lb_listener" "https" {
   load_balancer_arn = aws_lb.main.arn
   port              = 443
   protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-Res-PQ-2025-09"
+  # TODO: Upgrade to "ELBSecurityPolicy-TLS13-1-2-Res-PQ-2025-09" after the
+  # old http listener state entry is cleaned up (requires a successful deploy first).
+  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
   certificate_arn   = "arn:aws:acm:us-east-2:905418285725:certificate/2519ada5-98d9-43f5-9b31-e70801862222"
 
   default_action {
@@ -60,7 +62,7 @@ resource "aws_lb_listener" "https" {
   }
 }
 
-resource "aws_lb_listener" "http" {
+resource "aws_lb_listener" "http_redirect" {
   load_balancer_arn = aws_lb.main.arn
   port              = 80
   protocol          = "HTTP"
