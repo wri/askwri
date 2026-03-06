5afcd064e912ac831860047b146f69a9734f9785
Kinshuk Govil <Kinshuk.Govil@wri.org>
2026-03-06 11:46:40 -0800

    terraform fmt

--- terraform/infrastructure/alb.tf
+++ terraform/infrastructure/alb.tf
@@ -53,8 +53,8 @@ resource "aws_lb_listener" "https" {
   protocol          = "HTTPS"
   # TODO: Upgrade to "ELBSecurityPolicy-TLS13-1-2-Res-PQ-2025-09" after the
   # old http listener state entry is cleaned up (requires a successful deploy first).
-  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
-  certificate_arn   = "arn:aws:acm:us-east-2:905418285725:certificate/2519ada5-98d9-43f5-9b31-e70801862222"
+  ssl_policy      = "ELBSecurityPolicy-TLS13-1-2-2021-06"
+  certificate_arn = "arn:aws:acm:us-east-2:905418285725:certificate/2519ada5-98d9-43f5-9b31-e70801862222"
 
   default_action {
     type             = "forward"
