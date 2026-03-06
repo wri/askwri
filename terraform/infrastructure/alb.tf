34df6a90ca6fbf5e70fc3c5508709abe0f7015ad
Kinshuk Govil <Kinshuk.Govil@wri.org>
2026-03-06 15:05:49 -0800

    terraform fmt

--- terraform/infrastructure/alb.tf
+++ terraform/infrastructure/alb.tf
@@ -53,8 +53,8 @@ resource "aws_lb_listener" "https" {
   load_balancer_arn = aws_lb.main[0].arn
   port              = 443
   protocol          = "HTTPS"
-  ssl_policy      = "ELBSecurityPolicy-TLS13-1-2-Res-PQ-2025-09"
-  certificate_arn = var.certificate_arn
+  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-Res-PQ-2025-09"
+  certificate_arn   = var.certificate_arn
 
   default_action {
     type             = "forward"
