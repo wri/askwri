ade83bce23b87709dcff4fe1a2fb19007756c831
Kinshuk Govil <Kinshuk.Govil@wri.org>
2026-03-06 11:34:28 -0800

    Share ALB between QA and Prod
    Rename ALB and associated resources to remove environment

--- terraform/infrastructure/alb.tf
+++ terraform/infrastructure/alb.tf
@@ -63,7 +63,8 @@ resource "aws_lb_listener" "https" {
 }
 
 resource "aws_lb_listener" "http_redirect" {
-  load_balancer_arn = aws_lb.main.arn
+  count             = var.use_shared_vpc ? 0 : 1
+  load_balancer_arn = aws_lb.main[0].arn
   port              = 80
   protocol          = "HTTP"
 
