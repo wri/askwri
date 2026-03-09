6fa8f07d72bf48bb504a44ca0fdb86d68fcd6d0a
Carlos Pesina <pesinasiller@users.noreply.github.com>
2026-03-09 22:10:14 +0400

    adds noopener noreferrer

--- src/app/components/Footer/index.tsx
+++ src/app/components/Footer/index.tsx
@@ -5,18 +5,24 @@ import { Footer as WriFooter } from '@worldresources/wri-design-systems'
 
 export const Footer = () => (
   <WriFooter filled={false}>
-    <Link target='_blank' href='https://www.wri.org/about/privacy-policy'>
+    <Link
+      target='_blank'
+      href='https://www.wri.org/about/privacy-policy'
+      rel='noopener noreferrer'
+    >
       Privacy policy
     </Link>
     <Link
       target='_blank'
       href='https://www.wri.org/about/legal/general-terms-use'
+      rel='noopener noreferrer'
     >
       Terms of service
     </Link>
     <Link
       target='_blank'
       href='https://gfw.atlassian.net/wiki/external/YzBhMzJlMmExNWE3NDU3MjkwYWFiYzQ4YTNlMGQ0MWY'
+      rel='noopener noreferrer'
     >
       Methodology
     </Link>
