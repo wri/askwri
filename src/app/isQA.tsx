fe148fdf00bbabe63ab119ce8f61a0b3a3154e9f
Carlos Pesina <pesinasiller@users.noreply.github.com>
2026-03-16 12:25:13 +0500

    AW-73-add_qa_visual_clue_client_component

---
+++ src/app/isQA.tsx
@@ -0,0 +1,27 @@
+'use client'
+
+const IsQA = () => {
+  const isQa = process.env.NEXT_PUBLIC_ENVIRONMENT === 'qa'
+  if (!isQa) return null
+  return (
+    <div
+      style={{
+        backgroundColor: '#C11101',
+        color: 'white',
+        padding: '10px 20px',
+        textAlign: 'center',
+        position: 'fixed',
+        bottom: '3%',
+        right: '3%',
+        zIndex: 1000,
+        borderRadius: '16px',
+        pointerEvents: 'none',
+        userSelect: 'none',
+      }}
+    >
+      QA
+    </div>
+  )
+}
+
+export default IsQA
