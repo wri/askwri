b1bba8963328cd410b11486d950909b50b677b3b
Carlos Pesina <pesinasiller@users.noreply.github.com>
2026-03-25 16:15:53 +0500

    Revert "move hotjar ID to env variables (#134)" (#136)
    This reverts commit 0561a5e283559d47bba832016e1df7a5a72ebc62.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,20 +17,18 @@ const RootLayout = ({ children }: { children: ReactNode }) => (
   <html lang='en'>
     <head>
       {/* Hotjar Tracking Code for Ask WRI */}
-      {process.env.NEXT_PUBLIC_HOTJAR_ID && (
-        <Script id='hotjar' strategy='afterInteractive'>
-          {`
+      <Script id='hotjar' strategy='afterInteractive'>
+        {`
           (function(h,o,t,j,a,r){
             h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-            h._hjSettings={hjid:${process.env.NEXT_PUBLIC_HOTJAR_ID},hjsv:6};
+            h._hjSettings={hjid:6656811,hjsv:6};
             a=o.getElementsByTagName('head')[0];
             r=o.createElement('script');r.async=1;
             r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
             a.appendChild(r);
           })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
         `}
-        </Script>
-      )}
+      </Script>
     </head>
     <body>
       <ChakraProvider>
